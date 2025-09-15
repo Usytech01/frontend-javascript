176691a15e2c671172b4144af7cb0d68d9c03ba8
Usytech01 <usmandioo@yahoo.com>
2025-09-15 17:27:27 +0100

    correction

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,9 +1,21 @@
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
+interface Teacher{
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience: number;
+    location: string;
+    contract: boolean;
+    Teacher: any;
+}
+
+const teacher: Teacher = {
+    firstName: "John",
+    lastName: "Smit",
+    fullTimeEmployee: true,
+    location: "Lagos",
+    contract: true,
+    yearsOfExperience: 0,
+    Teacher: undefined
 };
 
-console.log(teacher3);
+console.log(teacher);
