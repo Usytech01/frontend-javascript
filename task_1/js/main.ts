556fd388c73a39a69e512120d5d478ac62491800
Usytech01 <usmandioo@yahoo.com>
2025-09-15 17:21:27 +0100

    add task 1

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,18 +1,9 @@
-interface teacher{
-    firstName: string;
-    lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience: number;
-    location: string;
-    contract: boolean;
-}
-
-const teacher1: teacher = {
-    firstName: "John",
-    lastName: "Smit",
-    fullTimeEmployee: true,
-    location: "Lagos",
-    contract: true
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
 };
 
-console.log(teacher1);
+console.log(teacher3);
